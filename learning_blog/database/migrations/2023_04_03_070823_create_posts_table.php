ff75f2aa493b7ab92ac6d747f52a3d130a90aac0
aline_pencreach <aline.pencreach@gmail.com>
2023-04-03 09:14:57 +0200

    property added in migrations

--- learning_blog/database/migrations/2023_04_03_070823_create_posts_table.php
+++ learning_blog/database/migrations/2023_04_03_070823_create_posts_table.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,7 +15,12 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string("title");
+            $table->text("content");
             $table->timestamps();
+
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Category::class);
         });
     }
 
